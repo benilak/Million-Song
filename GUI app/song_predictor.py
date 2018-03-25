a64ad075670b6ef45e6ea382502124b55e700a60
landonrs <altruist8@yahoo.com>
2018-03-24 19:43:07 -0600

    Added Youtube search functionality, Can now display individual songs

--- GUI app/song_predictor.py
+++ GUI app/song_predictor.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 from tkinter import *
 import pandas
 import sqlite3
+import webbrowser
 
 DATA_SET = pandas.read_csv("test_songsFinal.csv")
 ARTIST_NAMES = DATA_SET["artist_name"].iloc()
@@ -60,7 +61,7 @@ class Main_Menu(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (SearchPage, StartPage):
+        for F in (SearchPage, SongPage, StartPage):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -74,6 +75,38 @@ class Main_Menu(tk.Tk):
         frame = self.frames[page_name]
         frame.tkraise()
 
+    def display_artist_songs(self, artist):
+        # print(artist)
+        conn = sqlite3.connect('songs_subset.db')
+        conn.row_factory = lambda cursor, row: row[0]
+        cursor = conn.cursor()
+        # get songs that match artist
+        sql = "SELECT title from songs WHERE artist_name = '" + artist.strip() + "' ORDER BY title"
+        # print(sql)
+        cursor.execute(sql)
+        songs = cursor.fetchall()
+        # print(songs)
+        # first reset the song results field
+        frame = self.frames["SongPage"]
+        frame.song_result.delete(0, END)
+        # now insert the matched results
+        for match in songs:
+            output_text = str(match) + " "
+            frame.song_result.insert(END, output_text)
+        conn.close()
+        frame.current_artist = artist.strip()
+        # now display the song page
+        frame.tkraise()
+
+    def search_youtube(self, search):
+        search = search.strip()
+        query = search.replace(" ", "+")
+        print(query)
+        url = "https://www.youtube.com/results?search_query=" + query
+        webbrowser.open(url)
+
+
+
 
 
 class StartPage(tk.Frame):
@@ -81,7 +114,7 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="Welcome to the Song Predictor App", font=controller.title_font)
+        label = tk.Label(self, text="Welcome to the Song Recommendation App", font=controller.title_font)
         label.pack(side="top", fill="x")
 
         button1 = tk.Button(self, text="Search for an artist",
@@ -109,7 +142,7 @@ class SearchPage(tk.Frame):
         # drop_down = OptionMenu(self, variable, *OPTIONS)
         # drop_down.place(x=200, y=100)
         search_field = Entry(self)
-        search_field.place(x=200, y=100)
+        search_field.place(relx=.5, y=100, anchor=CENTER)
         search_button = tk.Button(self, text="Search for Artist",
                                   command=lambda: self.searchArtists(search_field.get()))
 
@@ -118,11 +151,13 @@ class SearchPage(tk.Frame):
         # attach scroll bar to search results box
         self.search_result.config(yscrollcommand=scrollbar.set)
         scrollbar.config(command=self.search_result.yview)
-
+        artist_select_button = tk.Button(self, text="View this artist's available songs",
+                            command=lambda: controller.display_artist_songs(self.search_result.selection_get()))
+        artist_select_button.place(relx=.5, rely=.85, anchor=CENTER)
 
         button1 = tk.Button(self, text="return to main menu",
                             command=lambda: controller.show_frame("StartPage"))
-        button1.place(relx=.5, rely=.9, anchor=CENTER)
+        button1.place(relx=.5, rely=.95, anchor=CENTER)
 
     def getArtistNames(self):
         conn = sqlite3.connect('songs_subset.db')
@@ -130,7 +165,7 @@ class SearchPage(tk.Frame):
         cursor = conn.cursor()
         cursor.execute("""SELECT DISTINCT(artist_name) from songs ORDER BY artist_name""")
         artists = cursor.fetchall()
-        print(artists[:100])
+        # print(artists[:100])
         conn.close()
         return artists
 
@@ -138,7 +173,7 @@ class SearchPage(tk.Frame):
         conn = sqlite3.connect('songs_subset.db')
         conn.row_factory = lambda cursor, row: row[0]
         cursor = conn.cursor()
-        print("we got HERE!")
+        # print("we got HERE!")
         cursor.execute("""SELECT DISTINCT(artist_name) from songs WHERE artist_name like ? ORDER BY artist_name""",
                        ('%' + search + '%',))
         artists = cursor.fetchall()
@@ -150,6 +185,37 @@ class SearchPage(tk.Frame):
                 output_text = str(match) + " "
                 self.search_result.insert(END, output_text)
         conn.close()
+
+
+class SongPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        # this holds the current artist whose songs we are viewing
+        self.current_artist = None
+        self.song_result = tk.Listbox(self)
+        self.song_result.config(width=75, height=10)
+        self.song_result.place(relx=.5, rely=.4, anchor=CENTER)
+        scrollbar = Scrollbar(self)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        label = tk.Label(self, text="Songs for this artist:", font=controller.title_font)
+        label.pack(side="top", fill="x")
+
+        # attach scroll bar to search results box
+        self.song_result.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.song_result.yview)
+        song_select_button = tk.Button(self, text="Find recommendations similar to this song",
+                                         command=lambda: controller.show_frame("StartPage"))
+        song_select_button.place(relx=.7, rely=.75, anchor=CENTER)
+
+        youtube_search_button = tk.Button(self, text="Search this song on youtube",
+                                       command=lambda: controller.search_youtube(self.current_artist + " " + self.song_result.selection_get()))
+
+        youtube_search_button.place(relx=.2, rely=.75, anchor=CENTER)
+
+        button1 = tk.Button(self, text="return to Artist search",
+                            command=lambda: controller.show_frame("SearchPage"))
+        button1.place(relx=.5, rely=.9, anchor=CENTER)
 
 
 
